28cd9d98dd4d4aa5cde7a5736bde2dbf788211d2
Thiru <114560047+Thiruop@users.noreply.github.com>
2025-05-18 22:40:16 +0530

    Delete src directory

--- src/types/employee.ts
+++
@@ -1,9 +0,0 @@
-export interface Employee {
-    EmployeeID: number;
-    EmployeeName: string;
-    Department: string;
-    Designation: string;
-    Project: string;
-    Type: string;
-    Status: string;
-} 
